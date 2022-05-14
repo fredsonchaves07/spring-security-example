5ee4d73a5e0156b75b05a9edbeb3e2bd85adaf3f
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-05-14 13:21:57 -0300

    feat: basic implementation spring security

---
+++ src/main/java/com/fredson/springsecutiryexample/security/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.fredson.springsecutiryexample.security;
+
+import com.fredson.springsecutiryexample.modules.user.entities.User;
+import com.fredson.springsecutiryexample.modules.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User existUser = userRepository.findByUsernameFetchRoles(username);
+        if (existUser != null) throw new Error("User does not exist!");
+        return UserPrincipal.create(existUser);
+    }
+}
--- src/main/java/com/fredson/springsecutiryexample/security/SecurityConfig.java
+++ src/main/java/com/fredson/springsecutiryexample/security/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.fredson.springsecutiryexample.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -13,5 +21,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    private BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
